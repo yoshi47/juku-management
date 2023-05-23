b8ee418b4d07f0fa54e778e0968fbebd276d7c25
yoshi47 <yoshix47@gmail.com>
2023-05-23 22:09:22 +0900

    :sparkles: dbを設計した

--- backend/management/admin.py
+++ backend/management/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Subject, Lesson
+
+
 # Register your models here.
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+
+class LessonAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Lesson, LessonAdmin)
